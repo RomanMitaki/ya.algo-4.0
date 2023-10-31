84ea51976be0c083c3a63e75b8647c99774f41f0
Roman <r.mitaki@yandex.ru>
2023-10-31 21:30:36 +0300

    Create warm-up.js

---
+++ warm-up.js
@@ -0,0 +1,235 @@
+//Не минимум на отрезке
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+const input = [];
+
+readline
+  .on('line', (line) => {
+    input.push(line);
+  })
+  .on('close', () => {
+    const res = solution(input);
+    console.log(res);
+    process.exit(0);
+  });
+  
+  function solution(input) {
+    let [data, arr, ...rest] = input;
+    const nums = arr.split(' ').map(Number);
+    const windows = rest.map(window => window.split(' ').map(Number));
+
+    const check = (window) => {
+        let [start, end] = window;
+        let min = nums[start];
+        for (let i = start; i <= end; i++) {
+            if (nums[i] > min) return nums[i];
+            if (nums[i] < min) return min;
+        }
+        return 'NOT FOUND'
+    }
+
+    const ans = windows.map(window => check(window));
+
+    return ans.join('\n')
+}
+
+//Сложить две дроби
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+readline.on('line', (line) => {
+  const result = solution(line);
+  console.log(result);
+  process.exit(0);
+});
+
+function solution(input) {
+  const data = input.split(" ").map(Number);
+  let [a, b, c, d] = data;
+  
+  let m = a * d + c * b;
+  let n = b * d;
+  
+  const findGcd = (num1, num2) => {
+    while (num1 !== 0 && num2 !== 0) {
+      if (Math.abs(num1) > Math.abs(num2)) {
+        num1 = num1 % num2;
+      } else {
+        num2 = num2 % num1;
+      }
+    }
+    return !num1 ? num2 : num1;
+  };
+
+  const gcd = findGcd(m, n);
+  let ans = [m / gcd, n / gcd];
+  return ans.join(" ");
+}
+
+//Анаграмма?
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+const input = [];
+
+readline
+  .on('line', (line) => {
+    input.push(line);
+  })
+  .on('close', () => {
+    const res = solution(input);
+    console.log(res);
+    process.exit(0);
+  });
+  
+function solution(words) {
+  const getHash = (word) => {
+    const ans = new Map();
+    for (let letter of word) {
+      ans.set(letter, (ans.get(letter) || 0) + 1)
+    }
+    return ans;
+  }
+  let [first, second] = words;
+  const mapFirst = getHash(first);
+  const mapSecond = getHash(second);
+
+  if (mapFirst.size !== mapSecond.size) return 'NO';
+
+  for (let key of mapFirst.keys()) {
+    if (mapFirst.get(key) !== mapSecond.get(key)) return 'NO';
+  }
+
+  return 'YES';
+}
+
+//Кролик учит геометрию
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+const input = [];
+
+readline
+  .on('line', (line) => {
+    input.push(line);
+  })
+  .on('close', () => {
+    const res = solution(input);
+    console.log(res);
+    process.exit(0);
+  });
+  
+  function solution(input) {
+  const [_, ...rest] = input;
+  const matrix = rest.map((el) => el.split(" ").map(Number));
+
+  const n = matrix.length;
+  const m = matrix[0].length;
+
+  const seen = [];
+  for (let i = 0; i < n; i++) {
+    seen.push(new Array(m).fill(0));
+  }
+
+  for (let i = 0; i < n; i++) {
+    seen[i][0] = matrix[i][0];
+  }
+
+  for (let i = 0; i < m; i++) {
+    seen[0][i] = matrix[0][i];
+  }
+
+  let ans = 0;
+
+  for (i = 1; i < n; i++) {
+    for (j = 1; j < m; j++) {
+      if (matrix[i][j] === 1) {
+        seen[i][j] =
+          Math.min(
+            seen[i][j - 1],
+            Math.min(seen[i - 1][j], seen[i - 1][j - 1])
+          ) + 1;
+        ans = Math.max(ans, seen[i][j]);
+      } else {
+        seen[i][j] = 0;
+      }
+    }
+  }
+
+  return ans;
+}
+
+//Результаты контеста
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+const input = [];
+
+readline
+  .on('line', (line) => {
+    input.push(line);
+  })
+  .on('close', () => {
+    const res = solution(input);
+    console.log(res);
+    process.exit(0);
+  });
+  
+  function solution(input) {
+  const data = input.map(Number);
+  let [groupA, groupB, tasks] = data;
+  let maxA = groupA;
+  let minB = Math.ceil(groupB / tasks);
+  if (maxA <= minB) return "No";
+  return 'Yes';
+}
+
+//Правильная скобочная последовательность
+
+const readline = require('readline').createInterface(
+  process.stdin,
+  process.stdout,
+);
+
+readline.on('line', (line) => {
+  const result = solution(line);
+  console.log(result);
+  process.exit(0);
+});
+
+function solution (str) {
+    const stack = [];
+    const map = new Map();
+    map.set('(', ')');
+    map.set('[', ']');
+    map.set('{', '}');
+
+    for (let i = 0; i < str.length; i++) {
+        if (!map.has(str[i])) {
+            if (!stack.length || map.get(stack[stack.length - 1]) !== str[i]) {
+                return 'no'
+            } else {
+                stack.pop();
+            }
+        } else {
+            stack.push(str[i]);
+        }
+    }
+    return stack.length ? 'no' : 'yes'
+}
